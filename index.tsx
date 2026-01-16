42f3e76d37c9fb35550e899d64651a2a3b105c35
Wei-Cheng-Hsiao <41099693+Wei-Cheng-Hsiao@users.noreply.github.com>
2026-01-16 14:36:24 +0800

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,16 +1,1 @@
-
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 所有邏輯已遷移至 index.html 之 <script type="text/babel"> 中以實現單一 HTML 檔案運行的需求。
